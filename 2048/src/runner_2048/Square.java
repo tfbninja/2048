d5532163ea0926a491571c8947bf382516febbbe
Tim Barber <tfbninja@users.noreply.github.com>
2019-04-15 13:08:24 -0600

    yeee

---
+++ 2048/src/runner_2048/Square.java
@@ -0,0 +1,62 @@
+package runner_2048;
+
+/**
+ *
+ * @author Tim Barber
+ */
+public class Square {
+
+    private int value;
+
+    public static int[] MIN_VALUES = {2, 4};
+
+    public Square() {
+        value = MIN_VALUES[0];
+    }
+
+    public Square(int value) {
+        this();
+        if (value > MIN_VALUES[0]) {
+            this.value = value;
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int val) {
+        if (value > MIN_VALUES[0]) {
+            this.value = val;
+        }
+    }
+
+    public int combine(Square other) {
+        value += other.value;
+        return value;
+    }
+}
+
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2019 Tim Barber.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
